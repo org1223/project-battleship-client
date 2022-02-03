d07977afae42d8f5797a502ff6d96c2fc669c671
WaylayOvercast <timelyovercast@gmail.com>
2022-02-03 15:29:44 -0600

    init commit with foundation setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { motion } from 'framer-motion';
+import { useRecoilValue, useRecoilState } from 'recoil';
+
+
+
+
+function App() {
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { RecoilRoot } from 'recoil'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <Router>
+        <App />
+      </Router>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
