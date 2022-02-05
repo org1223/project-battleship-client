87a8efa2d3cf29a4c2beb6d60a2a880968fff06f
FiftyShadesofCode <philiprmcdavid@gmail.com>
2022-02-04 22:27:02 -0500

    did the landing page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { RecoilRoot } from 'recoil'
+import {App} from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {RecoilRoot} from 'recoil'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <Router>
-        <App />
-      </Router>
-    </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <RecoilRoot>
+            <Router>
+                <App/>
+            </Router>
+        </RecoilRoot>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
